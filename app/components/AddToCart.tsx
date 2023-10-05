1d966e0d074b17f55ad10034a37142331dcd8827
PavelKazlov <6956717@gmail.com>
2023-10-05 15:08:32 +0200

    initial commit

---
+++ app/components/AddToCart.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+const AddToCart = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-primary"
+        onClick={() => {
+          console.log("helle");
+        }}
+      >
+        Add to Cart
+      </button>
+    </div>
+  );
+};
+
+export default AddToCart;
