32cf48fd8492c59ed1afd970e4b55b3209f8ed9d
PavelKazlov <6956717@gmail.com>
2023-10-06 13:54:19 +0200

    Added SQLdb

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -1,17 +1,41 @@
+import { prisma } from "@/prisma/clients";
 import { NextRequest, NextResponse } from "next/server";
 import schema from "./schema";
 
-export function GET(request: NextRequest) {
-  return NextResponse.json([
-    { id: 1, name: "Mosh" },
-    { id: 2, name: "Pavel" },
-  ]);
-}
+export async function GET(request: NextRequest) {
+  const users = await prisma.user
+    .findMany
+    // For adding filtration use object
+    //   {
+    //   where: {
+    //     email: "",
+    //   },
+    // }
+    ();
 
+  return NextResponse.json(users);
+}
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = schema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+    return NextResponse.json(validation.error.errors, {
+      status: 400,
+    });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json(newUser, { status: 201 });
 }
